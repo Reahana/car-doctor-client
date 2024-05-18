c73a8622cfb344ea94604afab1390348271ce120
Reahana <reahana233@gmail.com>
2024-05-18 12:49:02 +0600

    book service

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -3,6 +3,8 @@ import Main from "../layout/Main";
 import Home from "../pages/Home/Home/Home";
 import Login from "../pages/Login/Login";
 import SignUp from "../pages/Signup/SignUp";
+import CheckOut from "../pages/CheckOut/CheckOut";
+import BookService from "../pages/BookService/BookService";
 
 const router = createBrowserRouter([
     {
@@ -20,7 +22,21 @@ const router = createBrowserRouter([
         {
             path: 'signup', 
             element: <SignUp></SignUp>
-        }
+        },
+        // {
+        //   path: 'checkout/:id', 
+        //   element:<CheckOut></CheckOut>,
+        //   loader: ({params}) => fetch(`http://localhost:5000/services/${params.id}`)
+        // },
+        {
+          path: 'book/:id', 
+          element: <BookService></BookService>, 
+          loader: ({params}) => fetch(`http://localhost:5000/services/${params.id}`)
+        },
+        // {
+        //   path: 'bookings', 
+        //   element: <PrivateRoute><Bookings></Bookings></PrivateRoute>
+        // }
       ]
     },
   ]);
--- src/pages/Home/Services/Services.jsx
+++ src/pages/Home/Services/Services.jsx
@@ -6,7 +6,8 @@ const Services = () => {
     const [services, setServices] = useState([]);
 
     useEffect(() => {
-        fetch('services.json')
+        // fetch('services.json')
+        fetch('http://localhost:5000/services')
             .then(res => res.json())
             .then(data => setServices(data));
     }, [])
