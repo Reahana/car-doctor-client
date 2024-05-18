283267b34bf4ff79eb037917798e12a544c11911
Reahana <reahana233@gmail.com>
2024-05-19 00:08:44 +0600

    booking

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -4,6 +4,7 @@ import Home from "../pages/Home/Home/Home";
 import Login from "../pages/Login/Login";
 import SignUp from "../pages/Signup/SignUp";
 import BookService from "../pages/BookService/BookService";
+import Bookings from "../pages/Bookings/Bookings";
 
 const router = createBrowserRouter([
     {
@@ -22,20 +23,17 @@ const router = createBrowserRouter([
             path: 'signup', 
             element: <SignUp></SignUp>
         },
-        // {
-        //   path: 'checkout/:id', 
-        //   element:<CheckOut></CheckOut>,
-        //   loader: ({params}) => fetch(`http://localhost:5000/services/${params.id}`)
-        // },
+      
         {
           path: 'book/:id', 
           element: <BookService></BookService>, 
           loader: ({params}) => fetch(`http://localhost:5000/services/${params.id}`)
         },
-        // {
-        //   path: 'bookings', 
-        //   element: <PrivateRoute><Bookings></Bookings></PrivateRoute>
-        // }
+        {
+          path: 'bookings', 
+          //element: <PrivateRoute><Bookings></Bookings></PrivateRoute>
+          element:<Bookings></Bookings>
+        }
       ]
     },
   ]);
